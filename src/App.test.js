e6a2718f93ddfb0b243c71b060fd72ff0fe60c77
estefanianabella@gmail.com <estefanianabella@gmail.com>
2021-06-23 09:18:06 -0300

    add heroku stage  deploy2

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn este/i);
+  const linkElement = screen.getByText(/learn estefania/i);
   expect(linkElement).toBeInTheDocument();
 });
