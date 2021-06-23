05d2afb52c4d1d80276756bd0e57b841f744d760
estefanianabella@gmail.com <estefanianabella@gmail.com>
2021-06-23 09:27:58 -0300

    prueba falla 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn estefania
+          Learn estefi
         </a>
       </header>
     </div>
