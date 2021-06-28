d7c31645fa0123d546d089bfd2792c35da71850e
estefanianabella@gmail.com <estefanianabella@gmail.com>
2021-06-28 19:55:42 -0300

    probando 9

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn estefania/i);
+  const linkElement = screen.getByText(/learn estefania perez/i);
   expect(linkElement).toBeInTheDocument();
 });
