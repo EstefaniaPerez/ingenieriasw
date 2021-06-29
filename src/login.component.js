5465b8a15eb522559a74b946d7cc5954babf2344
estefanianabella@gmail.com <estefanianabella@gmail.com>
2021-06-29 09:38:35 -0300

    probando sonarcloud

--- src/login.component.js
+++ src/login.component.js
@@ -27,7 +27,10 @@ export default class Login extends Component {
                 </div>
                 <br></br>
 
-                
+                <button type="submit" className="btn btn-outline-primary">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot password?
+                </p>
               
             </form>
         );
